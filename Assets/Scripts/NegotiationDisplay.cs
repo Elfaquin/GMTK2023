7a75427789fe361b325152ac66712b04ae07c34f
Elfaquin <107775434+Elfaquin@users.noreply.github.com>
2023-07-09 18:35:43 +0200

    ouais trop bien les scales

--- Assets/Scripts/NegotiationDisplay.cs
+++ Assets/Scripts/NegotiationDisplay.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
-using UnityEditor.Build.Content;
 using UnityEngine;
 using UnityEngine.UI;
 
