41ff4322051692da9cf7616d05d36cc9aecdfd73
timothe.rosaz <timothe.rosaz@insa-rennes.fr>
2023-07-09 17:47:44 +0200

    le menu est là

---
+++ Assets/Scripts/StartGameButton.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameButton : MonoBehaviour {
+
+    public void StarGamePls() {
+        SceneManager.LoadScene(1);
+    }
+
+}
